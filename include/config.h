6a57d229a0fb905712b10fe4115de256dc21bd9e
Petr Štetiar <ynezz@true.cz>
2016-12-20 11:48:11 +0100

    Adding sources as recevied from OpenMesh
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

---
+++ include/config.h
@@ -0,0 +1,16 @@
+#define CONFIG_ATHEROS		1
+#define CONFIG_MACH_QCA955x	1
+#define CFG_INIT_STACK_IN_SRAM	1
+#define CONFIG_AP135	1
+#define __CONFIG_BOARD_NAME ap135
+#define CONFIG_BOARD_NAME "ap135"
+#define CFG_PLL_FREQ		CFG_PLL_720_600_240
+#define CONFIG_ATHRS17_PHY	1
+#define CFG_ATH_GMAC_NMACS	2
+#define CFG_ATH_GE1_IS_CONNECTED 1
+#define CONFIG_ATHRS_GMAC_SGMII  1
+#define FLASH_SIZE	16
+#define BOARD_NAME "MR1750"
+
+/* Automatically generated - do not edit */
+#include <configs/board955x.h>
